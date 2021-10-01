56c515224d85e3069632796eaf6ef6577d9d9742
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2021-10-01 13:48:38 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Owner", url="https://t.me/@SanilaRanatunga")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/@SanilaRanatunga")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nyou can download any youtube video using this bot.To download simply send the link of the video that you want to download😋�(A bot by @SanilaRanatunga)"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
