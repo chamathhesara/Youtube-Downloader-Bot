c281627f3cfdff59f7e255a0c703e42bae5ca97d
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2021-10-04 18:57:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,13 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Owner", url="https://t.me/@SanilaRanatunga")],
+        [InlineKeyboardButton("Owner", url="https://t.me/SanilaRanatunga")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/@SanilaRanatunga")]
+            "Report Bugs 😊", url="https://t.me/SanilaRanatunga")],
+         [InlineKeyboardButton("Torrent Download bot of me😎😋", url="https://t.me/torrentdownloader55_bot")],
+        [InlineKeyboardButton("Song Download bot of me😎😋", url="https://t.me/@songdownload597_bot")]
+        
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nYou can download any youtube video using this bot. To download simply send the link of the video that you want to download😋�(A bot by @SanilaRanatunga)"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nYou can download any youtube video using this bot. To download simply send the link of the video that you want to download😋(A bot by @SanilaRanatunga)"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
