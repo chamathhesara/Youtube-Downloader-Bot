acdf887ce3a2d3144639fc80d8b633d463b6d6e7
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2021-11-03 18:16:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,6 +11,7 @@ async def start(client, message):
          [InlineKeyboardButton("Torrent Downloader😎😋", url="https://t.me/torrentdownloader88_bot")],
         [InlineKeyboardButton("Song Downloader😎😋", url="https://t.me/songdownload597_bot")],
         [InlineKeyboardButton("Powerful Chat Bot😎😋", url="https://t.me/useful_powerful_chat_bot")]
+        [InlineKeyboardButton("Source Code🚥", url="https://github.com/sanila2007/Youtube-Downloader-Bot")]
         
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nYou can download any youtube video using this bot. To download simply send the link of the video that you want to download😋(A bot by @SanilaRanatunga)"
