959f0cee269fe9e4d30482ca67d455bcff06fa59
Sanila Ranatunga <88930336+sanila2007@users.noreply.github.com>
2021-12-02 22:43:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,15 +5,11 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Owner", url="https://t.me/SanilaRanatunga")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/SanilaRanatunga")],
-         [InlineKeyboardButton("Torrent Downloader🚠", url="https://t.me/torrentdownloader88_bot")],
-        [InlineKeyboardButton("Song Downloader🚠", url="https://t.me/songdownload597_bot")],
-        [InlineKeyboardButton("Powerful Chat Bot🚠", url="https://t.me/useful_powerful_chat_bot")],
+            "Report Bugs 😊", url="https://t.me/sanilaassistant_bot")],
         [InlineKeyboardButton("Source Code🚥", url="https://github.com/sanila2007/Youtube-Downloader-Bot")]
         
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nYou can download any youtube video using this bot. To download simply send the link of the video that you want to download😋(A bot by @SanilaRanatunga)"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\nYou can download any youtube video using this bot. To download simply send the link of the video that you want to download😋\n\n✨️A bot by @SanilaRanatunga"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
